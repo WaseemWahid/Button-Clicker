17da49a012a51ecc0cc89128ae029d2682845806
Waseem Wahid <wahid.waseem@gmail.com>
2021-12-20 13:50:47 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function signOut(element) {
+    element.innerText = "Logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function alertLike(element) {
+    alert("Ninja was liked");
+}
